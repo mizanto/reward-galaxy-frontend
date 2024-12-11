a401e4118d363507e32b6c5d74d920546a77970c
Sergey Bendak <sleepoke@gmail.com>
2024-12-11 12:45:23 +0100

    update store with slices

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice';
+import familyReducer from './familySlice';
+import goalsReducer from './goalsSlice';
 
 export const store = configureStore({
   reducer: {
-    // reducers will be added here
-  },
+    auth: authReducer,
+    family: familyReducer,
+    goals: goalsReducer
+  }
 });
